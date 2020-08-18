cee7926cfe0bf0a2b8aacd398b52198f5876b6dd
unknown <ponggas2@gmail.com>
2020-08-18 09:28:21 +0900

    라이브리 연습

---
+++ livere/src/test/java/com/jaybon/livere/LivereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaybon.livere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
